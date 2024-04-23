3e3f16555ec824fb981118d12a36bf1d2c1b7343
orgil <c0a23006cb@edu.teu.ac.jp>
2024-04-23 15:10:03 +0900

    renshuu3

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -13,9 +14,24 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect, または, 爆弾Rectの画面内外判定用の関数
+    引数: こうかとんRect, または, 爆弾Rect
+    戻り値: 横方向判定結果, 縦方向判定結果 (True: 画面内/False:画面外)
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
@@ -43,10 +59,17 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         
         bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
+        yoko, tate =check_bound(bd_rct)
+        if not yoko: #yoko houkou
+            vx *= -1
+        if not tate: # tate houkou
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
